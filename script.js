55b1887c0b7ec5147324cdad4c727bf7fb4e56b7
Brendan Lai <brendanlai00@gmail.com>
2024-03-13 03:45:33 -0400

    Create operate function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const calculator = document.querySelector('#container');
+let firstNum;
+let operator;
+let secondNum;
 
 function add(num1, num2){
     return num1 + num2;
@@ -14,4 +17,11 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return num1 / num2;
+}
+
+function operate(num1, operation, num2){
+    if(operation === "+") return add(num1, num2);
+    if(operation === '-') return subtract(num1, num2);
+    if(operation === '*') return multiply(num1, num2);
+    if(operation === '/') return divide(num1, num2);
 }
