2ec9c132e1427fd2d03f4134ff034db560810aed
Brendan Lai <brendanlai00@gmail.com>
2024-03-14 02:56:27 -0400

    Create setupButtons function for numbers

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const calculator = document.querySelector('#container');
+let calculator = document.querySelector('#container');
+let displayedResult = document.querySelector('#display');
+let buttons = document.querySelectorAll('#keypad button');
 let firstNum;
 let operator;
 let secondNum;
@@ -22,6 +24,17 @@ function divide(num1, num2){
 function operate(num1, operation, num2){
     if(operation === "+") return add(num1, num2);
     if(operation === '-') return subtract(num1, num2);
-    if(operation === '*') return multiply(num1, num2);
+    if(operation === 'x') return multiply(num1, num2);
     if(operation === '/') return divide(num1, num2);
 }
+
+function setupButtons(){
+    displayedResult.textContent = '0';
+    buttons.forEach((item) =>{
+        item.addEventListener('click', () =>{
+            displayedResult.textContent += item.textContent;
+        })
+    })
+}
+
+setupButtons();
