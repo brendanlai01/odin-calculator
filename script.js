3489d720d8576d25e99e1e0cb5f98148852ad143
Brendan Lai <brendanlai00@gmail.com>
2024-03-22 20:58:46 -0400

    Make calc functions use Number type

--- script.js
+++ script.js
@@ -7,19 +7,28 @@ let operator;
 let secondNum;
 
 function add(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 + num2;
 }
 
 function subtract(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 - num2;
 }
 
 function multiply(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 * num2;
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let sum = num1 / num2;
+    return Number(sum.toFixed(10));
 }
 
 function operate(num1, operation, num2){
